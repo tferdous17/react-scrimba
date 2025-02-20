8cd95c431e8ece07ad58cf7a15a38519c96dade0
tferdous17 <tferdous611@gmail.com>
2025-02-20 12:15:21 -0500

    create component files w/ stubs

---
+++ src/components/MainContent.jsx
@@ -0,0 +1,5 @@
+export default function MainContent() {
+    return (
+        <h1>Main goes here!</h1>
+    )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+    return (
+        <header>
+            <nav>
+                <h1>ReactFacts</h1>
+            </nav>
+        </header>
+    )
+}
